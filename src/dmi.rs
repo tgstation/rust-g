350efd5abfac63ce895f2356c11be6dc2a4c55db
Bjorn Neergaard <bjorn@neersighted.com>
2018-04-25 19:50:18 -0600

    Modularize strip_metadata()

--- src/dmi.rs
+++ src/dmi.rs
@@ -1,5 +1,6 @@
 use std::fs::File;
-use png::{Decoder, Encoder, HasParameters};
+
+use png::{Decoder, Encoder, HasParameters, OutputInfo};
 
 use error::Result;
 
@@ -8,17 +9,25 @@ byond_fn! { dmi_strip_metadata(path) {
 } }
 
 fn strip_metadata(path: &str) -> Result<()> {
-    // Read in the PNG, discarding metadata
-    let (info, mut reader) = Decoder::new(File::open(path)?).read_info()?;
-    let mut buffer = vec![0; reader.output_buffer_size()];
-    reader.next_frame(&mut buffer)?;
-    drop(reader);
+    let (info, image) = read_png(path)?;
+    write_png(path, info, image)?;
 
-    // Write the PNG back out
-    let mut encoder = Encoder::new(File::create(path)?, info.width, info.height);
-    encoder.set(info.color_type);
-    encoder.set(info.bit_depth);
-    let mut writer = encoder.write_header()?;
-    writer.write_image_data(&buffer)?;
     Ok(())
+}
+
+fn read_png(path: &str) -> Result<(OutputInfo, Vec<u8>)> {
+    let (info, mut reader) = Decoder::new(File::open(path)?).read_info()?;
+    let mut buf = Vec::with_capacity(info.buffer_size());
+    unsafe { buf.set_len(info.buffer_size()) }
+
+    reader.next_frame(&mut buf)?;
+    Ok((info, buf))
+}
+
+fn write_png(path: &str, info: OutputInfo, image: Vec<u8>) -> Result<()> {
+    let mut encoder = Encoder::new(File::create(path)?, info.width, info.height);
+    encoder.set(info.color_type).set(info.bit_depth);
+
+    let mut writer = encoder.write_header()?;
+    Ok(writer.write_image_data(&image)?)
 }
