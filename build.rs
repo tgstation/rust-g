af147ff6505f466e30f326dcbdf9bad0ad676f71
Sindorman <mikebykhovtsev@gmail.com>
2018-10-15 23:22:25 -0700

    Changing defines

--- build.rs
+++ build.rs
@@ -29,10 +29,11 @@ fn main() {
         write!(f, r#"
 #define rustg_file_read(fname) call(RUST_G, "file_read")(fname)
 #define rustg_file_write(text, fname) call(RUST_G, "file_write")(text, fname)
+#define rustg_file_append(text, fname) call(RUST_G, "file_append")(text, fname)
 
 #ifdef RUSTG_OVERRIDE_BUILTINS
 #define file2text(fname) rustg_file_read(fname)
-#define text2file(text, fname) rustg_file_write(text, fname)
+#define text2file(text, fname) rustg_file_append(text, fname)
 #endif
 "#).unwrap();
     }
