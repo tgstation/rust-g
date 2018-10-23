2357ddfde2f2ea9981ff0453ccaa7d4f00807e26
Cyberboss <Cyberboss@users.noreply.github.com>
2018-10-23 14:47:40 -0400

    The function I need to finish out of repo maps except I don't know how to code in rust

--- src/git.rs
+++ src/git.rs
@@ -1,5 +1,6 @@
 use git2::{Repository, Error, ErrorCode};
 use chrono::{Utc, TimeZone};
+use std::fs;
 
 thread_local! {
     static REPOSITORY: Result<Repository, Error> = Repository::open(".");
@@ -21,4 +22,29 @@ byond_fn! { rg_git_commit_date(rev) {
         let datetime = Utc.timestamp(commit.time().seconds(), 0);
         Ok(datetime.format("%F").to_string())
     }).ok()
+} }
+
+//ideally this shouldn't block BYOND somehow, probably by returning a job id or some shit
+//i'm having a time getting into rust
+//help fixing this is needful
+byond_fn! { get_repository_at_reference_start(repo_path, repo_url, rev) {
+
+    let mut repo = Repository::open(repo_path);
+
+    if(error)
+    {
+        //delete the shitty repo if it exists
+        fs::remove_dir_all(repo_path);  //on error, return the message
+
+        repo = Repository::clone(repo_url, repo_path);   //on error, return the message
+    }else{
+        //fetch origin
+        repo.fetch();  //on error, return the message
+    }
+    repo.checkout(rev);   //on error, return the message
+    return "SUCCESS";
+} }
+
+byond_fn! { get_repository_at_reference_start(job_id) {
+    return get_result_from_job_id_or_null_if_still_running(job_id);
 } }
