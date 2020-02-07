655a1a4a2e436504a9237e6d132ff7b26ec74455
Tad Hardesty <tad@platymuus.com>
2020-02-06 21:41:19 -0800

    Add dmi_create_png to rust_g.dm

--- build.rs
+++ build.rs
@@ -21,6 +21,7 @@ fn main() {
     if enabled!("DMI") {
         write!(f, r#"
 #define rustg_dmi_strip_metadata(fname) call(RUST_G, "dmi_strip_metadata")(fname)
+#define rustg_dmi_create_png(path, width, height, data) call(RUST_G, "dmi_create_png")(path, width, height, data)
 "#).unwrap();
     }
 
