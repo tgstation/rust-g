afba756ea28f428bc45744361e902035e2930228
Tad Hardesty <tad@platymuus.com>
2020-04-17 21:55:15 -0700

    Move genericity outside of byond_return

--- src/byond.rs
+++ src/byond.rs
@@ -20,12 +20,8 @@ pub fn parse_args<'a>(argc: c_int, argv: &'a *const *const c_char) -> Vec<Cow<'a
     }
 }
 
-pub fn byond_return<F, S>(inner: F) -> *const c_char
-where
-    F: FnOnce() -> Option<S>,
-    S: Into<Vec<u8>>,
-{
-    match inner() {
+pub fn byond_return(value: Option<Vec<u8>>) -> *const c_char {
+    match value {
         Some(string) => RETURN_STRING.with(|cell| {
             let cstring = CString::new(string).expect("null in returned string!");
             cell.replace(cstring);
@@ -42,7 +38,7 @@ macro_rules! byond_fn {
         pub unsafe extern "C" fn $name(
             _argc: ::std::os::raw::c_int, _argv: *const *const ::std::os::raw::c_char
         ) -> *const ::std::os::raw::c_char {
-            $crate::byond::byond_return(|| $body)
+            $crate::byond::byond_return((|| $body)().map(From::from))
         }
     };
 
@@ -59,7 +55,7 @@ macro_rules! byond_fn {
                 __argn += 1;
             )*
 
-            $crate::byond::byond_return(|| $body)
+            $crate::byond::byond_return((|| $body)().map(From::from))
         }
     };
 
