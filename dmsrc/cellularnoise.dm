d07e47ed8e4a89d4382fb951466e9d6867a0ad2f
Tad Hardesty <tad@platymuus.com>
2020-12-01 17:28:06 -0800

    Add width and height parameters to cnoise_generate

--- dmsrc/cellularnoise.dm
+++ dmsrc/cellularnoise.dm
@@ -1,8 +1,15 @@
 /**
- * This proc generates a cellular automata noise grid which can be used in procedural generation methods. Returns a single string that goes row by row, with values of 1 representing an alive cell, and a value of 0 representing a dead cell.
-* * percentage (required) The chance of a turf starting closed
-* * smoothing_iterations(required) The amount of iterations the cellular automata simulates before returning the results
-* * birth_limit (required) If the number of neighboring cells is higher than this amount, a cell is born
-* * death_limit (required) If the number of neighboring cells is lower than this amount, a cell dies
-*/
-#define rustg_cnoise_generate(percentage, smoothing_iterations, birth_limit, death_limit) call(RUST_G, "cnoise_generate")(percentage, smoothing_iterations, birth_limit, death_limit)
+ * This proc generates a cellular automata noise grid which can be used in procedural generation methods.
+ *
+ * Returns a single string that goes row by row, with values of 1 representing an alive cell, and a value of 0 representing a dead cell.
+ *
+ * Arguments:
+ * * percentage: The chance of a turf starting closed
+ * * smoothing_iterations: The amount of iterations the cellular automata simulates before returning the results
+ * * birth_limit: If the number of neighboring cells is higher than this amount, a cell is born
+ * * death_limit: If the number of neighboring cells is lower than this amount, a cell dies
+ * * width: The width of the grid.
+ * * height: The height of the grid.
+ */
+#define rustg_cnoise_generate(percentage, smoothing_iterations, birth_limit, death_limit, width, height) \
+    call(RUST_G, "cnoise_generate")(percentage, smoothing_iterations, birth_limit, death_limit, width, height)
--- src/cellularnoise.rs
+++ src/cellularnoise.rs
@@ -2,8 +2,8 @@ use crate::error::Result;
 use rand::*;
 use std::fmt::Write;
 
-byond_fn! { cnoise_generate(precentage,smoothing_iterations, birth_limit, death_limit) {
-    noise_gen(precentage, smoothing_iterations, birth_limit, death_limit).ok()
+byond_fn! { cnoise_generate(precentage,smoothing_iterations, birth_limit, death_limit, width, height) {
+    noise_gen(precentage, smoothing_iterations, birth_limit, death_limit, width, height).ok()
 } }
 
 fn noise_gen(
@@ -11,60 +11,61 @@ fn noise_gen(
     smoothing_level_as_str: &str,
     birth_limit_as_str: &str,
     death_limit_as_str: &str,
+    width_as_str: &str,
+    height_as_str: &str,
 ) -> Result<String> {
     let prec = prec_as_str.parse::<i32>()?;
     let smoothing_level = smoothing_level_as_str.parse::<i32>()?;
     let birth_limit = birth_limit_as_str.parse::<i32>()?;
     let death_limit = death_limit_as_str.parse::<i32>()?;
-    //Noise generation
+    let width = width_as_str.parse::<usize>()?;
+    let height = height_as_str.parse::<usize>()?;
 
-    let mut zplane = vec![vec![false; 254]; 254]; // 254 but we start at 0, and since byond starts at one it is 255 byond wise.
-    for i in 0..zplane.len() {
-        for j in 0..zplane[i].len() {
-            if rand::thread_rng().gen_range(0, 100) > prec {
-                zplane[i][j] = true;
-            }
+    // Noise generation
+    let mut zplane = vec![vec![false; width]; height];
+    for row in zplane.iter_mut() {
+        for cell in row.iter_mut() {
+            *cell = rand::thread_rng().gen_range(0, 100) > prec;
         }
     }
 
-    //Smoothing part
-
+    // Smoothing part
     for _timer in 0..smoothing_level {
         let zplane_old = zplane.clone();
-        for i in 0..zplane_old.len() {
-            for j in 0..zplane_old[i].len() {
+        for i in 0..height {
+            for j in 0..width {
                 let mut sum = 0;
 
-                if j > 0 {
+                if i > 0 {
+                    if j > 0 {
+                        sum += if zplane_old[i - 1][j - 1] { 1 } else { 0 };
+                    }
+
                     sum += if zplane_old[i - 1][j] { 1 } else { 0 };
+
+                    if j + 1 < width {
+                        sum += if zplane_old[i - 1][j + 1] { 1 } else { 0 };
+                    }
                 }
 
-                if i > 0 {
+                if j > 0 {
                     sum += if zplane_old[i][j - 1] { 1 } else { 0 };
                 }
 
-                if i > 0 && j > 0 {
-                    sum += if zplane_old[i - 1][j - 1] { 1 } else { 0 };
-                }
-
-                if j < zplane_old[i].len() - 1 {
-                    sum += if zplane_old[i + 1][j] { 1 } else { 0 };
-                }
-
-                if i < zplane_old.len() - 1 {
+                if j + 1 < width {
                     sum += if zplane_old[i][j + 1] { 1 } else { 0 };
                 }
 
-                if i < zplane_old.len() - 1 && j < zplane_old.len() - 1 {
-                    sum += if zplane_old[i + 1][j + 1] { 1 } else { 0 };
-                }
+                if i + 1 < height {
+                    if j > 0 {
+                        sum += if zplane_old[i + 1][j - 1] { 1 } else { 0 };
+                    }
 
-                if i > 0 && j < zplane_old[i].len() - 1 {
-                    sum += if zplane_old[i + 1][j - 1] { 1 } else { 0 };
-                }
+                    sum += if zplane_old[i + 1][j] { 1 } else { 0 };
 
-                if j > 0 && i < zplane_old.len() - 1 {
-                    sum += if zplane_old[i - 1][j + 1] { 1 } else { 0 };
+                    if j + 1 < width {
+                        sum += if zplane_old[i + 1][j + 1] { 1 } else { 0 };
+                    }
                 }
 
                 if zplane_old[i][j] == true {
@@ -84,13 +85,13 @@ fn noise_gen(
         }
     }
 
-    let mut string: String = String::from("");
-    for i in 0..zplane.len() {
-        for j in 0..zplane[i].len() {
-            if zplane[i][j] == true {
-                write!(string, "1");
+    let mut string = String::new();
+    for row in zplane.iter() {
+        for cell in row.iter() {
+            if *cell {
+                let _ = write!(string, "1");
             } else {
-                write!(string, "0");
+                let _ = write!(string, "0");
             }
         }
     }
