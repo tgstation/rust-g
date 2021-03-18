01272c520b056b0cc9f6439add85684205fea8f7
ZeWaka <zewakagamer@gmail.com>
2021-03-18 09:27:33 -0700

    question mark operator is useless here

--- src/dmi.rs
+++ src/dmi.rs
@@ -27,7 +27,7 @@ byond_fn! { dmi_resize_png(path, width, height, resizetype) {
 
 fn strip_metadata(path: &str) -> Result<()> {
     let (info, image) = read_png(path)?;
-    Ok(write_png(path, info, image)?)
+    Ok(write_png(path, info, image))
 }
 
 fn read_png(path: &str) -> Result<(OutputInfo, Vec<u8>)> {
