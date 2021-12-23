d5e7463d7551ab404404844bc1a2e42bd6049ed0
vuonojenmustaturska <naksu@youzen.ext.b2.fi>
2021-12-23 20:30:11 +0200

    radical good speed

--- src/lib.rs
+++ src/lib.rs
@@ -36,6 +36,8 @@ pub mod unzip;
 pub mod url;
 #[cfg(feature = "worleynoise")]
 pub mod worleynoise;
+#[cfg(feature = "acreplace")]
+pub mod acreplace;
 
 #[cfg(not(target_pointer_width = "32"))]
 compile_error!("rust-g must be compiled for a 32-bit target");
