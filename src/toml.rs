66260ff34c6682291c7ffc126831964ebb500b19
tralezab <40974010+tralezab@users.noreply.github.com>
2022-07-21 16:34:19 -0700

    toml2json now returns errors to byond (#98)
    Co-authored-by: ZeWaka <zewakagamer@gmail.com>

--- src/toml.rs
+++ src/toml.rs
@@ -1,7 +1,16 @@
 use crate::error::Result;
 
 byond_fn!(fn toml_file_to_json(path) {
-    toml_file_to_json_impl(path).ok()
+    serde_json::to_string(
+        &match toml_file_to_json_impl(path) {
+            Ok(value) => serde_json::json!({
+                "success": true, "content": value
+            }),
+            Err(error) => serde_json::json!({
+                "success": false, "content": error.to_string()
+            }),
+        }
+    ).ok()
 });
 
 fn toml_file_to_json_impl(path: &str) -> Result<String> {
