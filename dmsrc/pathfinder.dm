6676042f5e6dfb0ae16703729f3802c40b293790
Bastien Souchu <bsouchu@gmail.com>
2022-08-02 13:57:22 +0200

    allow to add and remove nodes

--- dmsrc/pathfinder.dm
+++ dmsrc/pathfinder.dm
@@ -1,2 +1,4 @@
 #define rustg_register_nodes(json) call(RUST_G, "register_nodes")(json)
 #define rustg_astar_generate_path(start_node_id, goal_node_id) call(RUST_G, "astar_generate_path")(start_node_id, goal_node_id)
+#define rustg_add_node(json) call(RUST_G, "add_node")(json)
+#define rustg_remove_node(unique_id) call(RUST_G, "remove_node")(unique_id)
--- src/pathfinder.rs
+++ src/pathfinder.rs
@@ -1,10 +1,11 @@
+use std::num::ParseIntError;
 use lazy_static::lazy_static;
 use mut_static::MutStatic;
 use num::integer::sqrt;
 use pathfinding::prelude::astar;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
-#[derive(Deserialize, Default, Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
+#[derive(Serialize, Deserialize, Default, Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Node {
     unique_id: usize,
     x: usize,
@@ -12,11 +13,34 @@ struct Node {
     connected_nodes_id: Vec<usize>,
 }
 
+/* fn main() {
+    let json= std::fs::read_to_string("ai_nodes_info.json").unwrap();
+    match register_nodes_(&json) {
+        Ok(_) => (),
+        Err(e) => println!("Error :{e}")
+    }
+    match remove_node_("15") {
+        Ok(_) => (),
+        Err(e) => println!("Error: {e}")
+    }
+    if NODES.read().unwrap().get(15).unwrap().is_some() {
+        print!("Error: node not deleted")
+    }
+    let mut node_to_add = NODES.read().unwrap().get(18).unwrap().clone().unwrap();
+    node_to_add.unique_id = NODES.read().unwrap().len();
+    let node_to_add = serde_json::to_string(&node_to_add).unwrap();
+    match add_node_(&node_to_add) {
+        Ok(_) => (),
+        Err(e) => println!("Error :{e}")
+    }
+} */
+
 impl Node {
     fn successors(&self) -> Vec<(Self, usize)> {
         self.connected_nodes_id
             .iter()
             .filter_map(|id| NODES.read().unwrap().get(*id).cloned())
+            .flatten()
             .map(|node| (node.clone(), self.distance(&node)))
             .collect()
     }
@@ -27,7 +51,7 @@ impl Node {
 }
 
 lazy_static! {
-    static ref NODES: MutStatic<Vec<Node>> = MutStatic::new();
+    static ref NODES: MutStatic<Vec<Option<Node>>> = MutStatic::new();
 }
 
 pub enum RegisteringNodesError {
@@ -47,22 +71,28 @@ impl From<mut_static::Error> for RegisteringNodesError {
     }
 }
 
+impl std::fmt::Display for RegisteringNodesError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RegisteringNodesError::MutexError(e) => write!(f, "Mutex error : {e}"),
+            RegisteringNodesError::SerdeError(e) => write!(f, "Parsing error : {e}"),
+            RegisteringNodesError::NodesNotCorrectlyIndexed => write!(f, "Node not indexed properly")
+        }
+    }
+}
+
 byond_fn!(fn register_nodes(json) {
     match register_nodes_(json) {
         Ok(s) => Some(s),
-        Err(e) => Some(match e {
-            RegisteringNodesError::MutexError(e) => format!("Mutex error : {}", e),
-            RegisteringNodesError::SerdeError(e) => format!("Parsing error : {}", e),
-            RegisteringNodesError::NodesNotCorrectlyIndexed => "Nodes not sorted".to_string()
-        })
+        Err(e) => Some(format!("{e}"))
     }
 });
 
 fn register_nodes_(json: &str) -> Result<String, RegisteringNodesError> {
-    let mut nodes: Vec<Node> = serde_json::from_str(json)?;
-    nodes.sort();
+    let nodes: Vec<Option<Node>> = serde_json::from_str(json)?;
     if nodes
         .iter()
+        .flatten()
         .enumerate()
         .filter(|(i, node)| i != &node.unique_id)
         .count()
@@ -74,6 +104,95 @@ fn register_nodes_(json: &str) -> Result<String, RegisteringNodesError> {
         return Err(RegisteringNodesError::MutexError(e));
     }
     Ok("1".to_string())
+}
+
+byond_fn!(fn add_node(json) {
+    match add_node_(json) {
+        Ok(s) => Some(s),
+        Err(e) => Some(format!("{e}"))
+    }
+});
+
+fn add_node_(json: &str) -> Result<String, RegisteringNodesError> {
+    let new_node: Node = serde_json::from_str(json)?;
+    if new_node.unique_id != NODES.read()?.len() {
+        return Err(RegisteringNodesError::NodesNotCorrectlyIndexed);
+    }
+    new_node.connected_nodes_id.iter().for_each(|i| {
+        if let Some(Some(node)) = NODES.write().unwrap().get_mut(*i) {
+            node.connected_nodes_id.push(new_node.unique_id);
+        }
+    });
+    NODES.write()?.push(Some(new_node));
+    Ok("1".to_string())
+}
+
+pub enum DeleteNodeError {
+    ParsingError(ParseIntError),
+    MutexError(mut_static::Error),
+    NodeNotFound,
+}
+
+impl From<ParseIntError> for DeleteNodeError {
+    fn from(error: ParseIntError) -> Self {
+        DeleteNodeError::ParsingError(error)
+    }
+}
+
+impl From<mut_static::Error> for DeleteNodeError {
+    fn from(error: mut_static::Error) -> Self {
+        DeleteNodeError::MutexError(error)
+    }
+}
+
+impl std::fmt::Display for DeleteNodeError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DeleteNodeError::MutexError(e) => write!(f, "Mutex error : {e}"),
+            DeleteNodeError::ParsingError(e) => write!(f, "Parsing error : {e}"),
+            DeleteNodeError::NodeNotFound => write!(f, "Node not found")
+        }
+    }
+}
+
+byond_fn!(fn remove_node(unique_id) {
+    match remove_node_(unique_id) {
+        Ok(s) => Some(s),
+        Err(e) => Some(format!("{e}"))
+    }
+});
+
+fn remove_node_(unique_id: &str) -> Result<String, DeleteNodeError> {
+    let unique_id = unique_id.parse::<usize>()?;
+    let mut nodes = NODES.write()?;
+
+    let node_to_delete = nodes.get(unique_id);
+    if node_to_delete.is_none() || node_to_delete.unwrap().is_none() {
+        return Err(DeleteNodeError::NodeNotFound);
+    }
+    let node_to_delete = node_to_delete.unwrap().clone().unwrap();
+    node_to_delete.connected_nodes_id.iter().for_each(|i| {
+        if let Some(Some(node)) = nodes.get_mut(*i) {
+            node.connected_nodes_id.retain(|id| id != i)
+        }
+    });
+
+    let node = nodes.get_mut(unique_id).unwrap();
+    *node = None;
+    Ok("1".to_string())
+}
+
+pub enum AstarError {
+    MutexError(mut_static::Error),
+    StartNodeNotFound,
+    GoalNodeNotFound,
+    NoPathFound,
+}
+
+impl From<mut_static::Error> for AstarError {
+    fn from(error: mut_static::Error) -> Self {
+        AstarError::MutexError(error)
+    }
 }
 
 byond_fn!(fn astar_generate_path(start_node_id, goal_node_id) {
@@ -96,19 +215,6 @@ byond_fn!(fn astar_generate_path(start_node_id, goal_node_id) {
     }
 });
 
-pub enum AstarError {
-    MutexError(mut_static::Error),
-    StartNodeNotFound,
-    GoalNodeNotFound,
-    NoPathFound,
-}
-
-impl From<mut_static::Error> for AstarError {
-    fn from(error: mut_static::Error) -> Self {
-        AstarError::MutexError(error)
-    }
-}
-
 fn astar_generate_path_(
     start_node_id: usize,
     goal_node_id: usize,
@@ -116,20 +222,20 @@ fn astar_generate_path_(
     let nodes = NODES.read()?;
 
     let start_node = nodes.get(start_node_id);
-    if start_node.is_none() {
+    if start_node.is_none() || start_node.unwrap().is_none() {
         return Err(AstarError::StartNodeNotFound);
     }
 
     let goal_node = nodes.get(goal_node_id);
-    if goal_node.is_none() {
+    if goal_node.is_none() || goal_node.unwrap().is_none() {
         return Err(AstarError::GoalNodeNotFound);
     }
 
     let path = astar(
-        start_node.unwrap(),
+        &start_node.unwrap().clone().unwrap(),
         |node| node.successors(),
-        |node| node.distance(goal_node.unwrap()),
-        |node| node == goal_node.unwrap(),
+        |node| node.distance(&goal_node.unwrap().clone().unwrap()),
+        |node| node == &goal_node.unwrap().clone().unwrap(),
     );
     if path.is_none() {
         return Err(AstarError::NoPathFound);
