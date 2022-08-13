ee3490968485b89fe72f14400d7520c9b5e55366
Bastien Souchu <bsouchu@gmail.com>
2022-08-13 23:18:27 +0200

    Less cloning, more doc

--- src/pathfinder.rs
+++ src/pathfinder.rs
@@ -1,39 +1,87 @@
-use lazy_static::lazy_static;
-use mut_static::MutStatic;
 use num::integer::sqrt;
 use pathfinding::prelude::astar;
 use serde::{Deserialize, Serialize};
+use std::borrow::Borrow;
+use std::hash::Hash;
 use std::num::ParseIntError;
+use std::sync::{Arc, RwLock};
+use std::usize::MAX;
+
+// static mut are not thread safe, and manipulating one is only possible behind unsafe tag
+// However, most of the unsafe call should be thread safe because nodes inside this vec are behind a RwLock
+static mut NODES: Vec<NodeContainer> = Vec::new();
+
+// Container for a node. Exist mainly to be able to implement Hash, which is not implemented for RefCell
+#[derive(Clone)]
+struct NodeContainer {
+    node: Arc<RwLock<Option<Node>>>,
+}
+
+impl Eq for NodeContainer {}
+
+impl PartialEq for NodeContainer {
+    fn eq(&self, other: &Self) -> bool {
+        self.node
+            .read()
+            .unwrap()
+            .eq(other.node.read().unwrap().borrow())
+    }
+}
+
+impl Hash for NodeContainer {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        self.node.read().unwrap().hash(state)
+    }
+}
+
+impl NodeContainer {
+    fn new(node: Node) -> Self {
+        let node = Arc::new(RwLock::new(Some(node)));
+        NodeContainer { node }
+    }
+
+    fn new_empty() -> Self {
+        let node = Arc::new(RwLock::new(None));
+        NodeContainer { node }
+    }
+}
 
 #[derive(Serialize, Deserialize, Default, Clone, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Node {
+    // A unique id that acts as its index in NODES
     unique_id: usize,
+    // Position of the node in byond
     x: usize,
     y: usize,
     z: usize,
+    // Indexes of nodes connected to this one
     connected_nodes_id: Vec<usize>,
 }
 
 impl Node {
-    fn successors(&self) -> Vec<(Self, usize)> {
+    // Return a vector of all connected nodes, encapsulated in a NodeContainer.
+    fn successors(&self) -> Vec<(NodeContainer, usize)> {
         self.connected_nodes_id
             .iter()
-            .filter_map(|id| NODES.read().unwrap().get(*id).cloned())
+            .map(|index| unsafe { NODES.get(*index) })
             .flatten()
-            .map(|node| (node.clone(), self.distance(&node)))
+            .map(|node_container| {
+                (
+                    // We create a new Arc to be able to share ownership without having to clone the node itself
+                    node_container.clone(),
+                    self.distance(node_container.node.read().unwrap().as_ref().unwrap()),
+                )
+            })
             .collect()
     }
 
+    // Return the geometric distance between this node and another one.
     fn distance(&self, other: &Self) -> usize {
         sqrt(
             ((self.x as isize - other.x as isize).pow(2)
                 + (self.y as isize - other.y as isize).pow(2)) as usize,
         )
     }
-}
-
-lazy_static! {
-    static ref NODES: MutStatic<Vec<Option<Node>>> = MutStatic::new();
 }
 
 enum RegisteringNodesError {
@@ -47,6 +95,7 @@ impl From<serde_json::Error> for RegisteringNodesError {
         RegisteringNodesError::SerdeError(error)
     }
 }
+
 impl From<mut_static::Error> for RegisteringNodesError {
     fn from(error: mut_static::Error) -> Self {
         RegisteringNodesError::MutexError(error)
@@ -65,18 +114,20 @@ impl std::fmt::Display for RegisteringNodesError {
     }
 }
 
-byond_fn!(fn register_nodes(json) {
-    match register_nodes_(json) {
+byond_fn!(fn register_nodesastar(json) {
+    match register_nodes(json) {
         Ok(s) => Some(s),
         Err(e) => Some(format!("{e}"))
     }
 });
 
-fn register_nodes_(json: &str) -> Result<String, RegisteringNodesError> {
-    let nodes: Vec<Option<Node>> = serde_json::from_str(json)?;
+// Builds a list of nodes from a json file.
+// Errors if the input list of nodes is not correctly indexed. Each node should have for unique id its position in the list, with the first unique-id being 0.
+// Memory safety not guaranteed in multithread environment
+fn register_nodes(json: &str) -> Result<String, RegisteringNodesError> {
+    let nodes: Vec<Node> = serde_json::from_str(json)?;
     if nodes
         .iter()
-        .flatten()
         .enumerate()
         .filter(|(i, node)| i != &node.unique_id)
         .count()
@@ -84,30 +135,44 @@ fn register_nodes_(json: &str) -> Result<String, RegisteringNodesError> {
     {
         return Err(RegisteringNodesError::NodesNotCorrectlyIndexed);
     }
-    if let Err(e) = NODES.set(nodes) {
-        return Err(RegisteringNodesError::MutexError(e));
-    }
+
+    nodes
+        .into_iter()
+        .for_each(|node| unsafe { NODES.push(NodeContainer::new(node)) });
+
     Ok("1".to_string())
 }
 
-byond_fn!(fn add_node(json) {
-    match add_node_(json) {
+byond_fn!(fn add_nodeastart(json) {
+    match add_node(json) {
         Ok(s) => Some(s),
         Err(e) => Some(format!("{e}"))
     }
 });
 
-fn add_node_(json: &str) -> Result<String, RegisteringNodesError> {
+// Add a node to the static list of node.
+// If it is connected to other existing nodes, it will update their connected_nodes_id list.
+fn add_node(json: &str) -> Result<String, RegisteringNodesError> {
     let new_node: Node = serde_json::from_str(json)?;
-    if new_node.unique_id != NODES.read()?.len() {
+
+    // As always, a node unique id should correspond to its index in NODES
+    if new_node.unique_id != unsafe { NODES.len() } {
         return Err(RegisteringNodesError::NodesNotCorrectlyIndexed);
     }
-    new_node.connected_nodes_id.iter().for_each(|i| {
-        if let Some(Some(node)) = NODES.write().unwrap().get_mut(*i) {
-            node.connected_nodes_id.push(new_node.unique_id);
-        }
+
+    // Make sure every connection we have we other nodes is 2 ways
+    new_node.connected_nodes_id.iter().for_each(|index| {
+        if let Some(node_container) = unsafe { NODES.get_mut(*index) } {
+            if let Some(node) = node_container.node.write().unwrap().as_mut() {
+                node.connected_nodes_id.push(new_node.unique_id)
+            }
+        };
     });
-    NODES.write()?.push(Some(new_node));
+
+    unsafe {
+        NODES.push(NodeContainer::new(new_node));
+    }
+
     Ok("1".to_string())
 }
 
@@ -139,30 +204,45 @@ impl std::fmt::Display for DeleteNodeError {
     }
 }
 
-byond_fn!(fn remove_node(unique_id) {
-    match remove_node_(unique_id) {
+byond_fn!(fn remove_nodeastar(unique_id) {
+    match remove_node(unique_id) {
         Ok(s) => Some(s),
         Err(e) => Some(format!("{e}"))
     }
 });
 
-fn remove_node_(unique_id: &str) -> Result<String, DeleteNodeError> {
+// Replace the node with unique_id by None
+// Update connected nodes as well so nothing target the removed node anymore
+// Errors if no node can be found with unique_id
+fn remove_node(unique_id: &str) -> Result<String, DeleteNodeError> {
     let unique_id = unique_id.parse::<usize>()?;
-    let mut nodes = NODES.write()?;
 
-    let node_to_delete = nodes.get(unique_id);
-    if node_to_delete.is_none() || node_to_delete.unwrap().is_none() {
-        return Err(DeleteNodeError::NodeNotFound);
-    }
-    let node_to_delete = node_to_delete.unwrap().clone().unwrap();
-    node_to_delete.connected_nodes_id.iter().for_each(|i| {
-        if let Some(Some(node)) = nodes.get_mut(*i) {
-            node.connected_nodes_id.retain(|id| id != i)
-        }
-    });
+    {
+        let node_to_delete_container = unsafe { NODES.get(unique_id) };
 
-    let node = nodes.get_mut(unique_id).unwrap();
-    *node = None;
+        let node_to_delete_ref = match node_to_delete_container {
+            None => return Err(DeleteNodeError::NodeNotFound),
+            Some(node_container) => node_container.node.write().unwrap(),
+        };
+
+        let node_to_delete = match node_to_delete_ref.as_ref() {
+            None => return Err(DeleteNodeError::NodeNotFound),
+            Some(node) => node,
+        };
+
+        // Erase all links to the removed node
+        node_to_delete.connected_nodes_id.iter().for_each(|i| {
+            if let Some(node_container) = unsafe { NODES.get_mut(*i) } {
+                if let Some(node) = node_container.node.write().unwrap().as_mut() {
+                    node.connected_nodes_id
+                        .retain(|index| index != &node_to_delete.unique_id);
+                }
+            }
+        });
+    } // We need to drop everything before set the removed node to None. This is to ensure memory safety
+
+    unsafe { NODES[unique_id] = NodeContainer::new_empty() }
+
     Ok("1".to_string())
 }
 
@@ -199,68 +279,78 @@ byond_fn!(fn astar_generate_path(start_node_id, goal_node_id) {
     }
 });
 
+// Compute the shortest path between start node and goal node using A*
 fn astar_generate_path_(
     start_node_id: usize,
     goal_node_id: usize,
 ) -> Result<Vec<usize>, AstarError> {
-    let nodes = NODES.read()?;
+    // Get the container of the start node. Errors if the start node cannot be found or is none
+    let start_node_container = match unsafe { NODES.get(start_node_id) } {
+        None => return Err(AstarError::StartNodeNotFound),
+        Some(node_container) => match node_container.node.read().unwrap().as_ref() {
+            None => return Err(AstarError::StartNodeNotFound),
+            Some(_) => node_container,
+        },
+    };
 
-    let start_node = nodes.get(start_node_id);
-    if start_node.is_none() || start_node.unwrap().is_none() {
-        return Err(AstarError::StartNodeNotFound);
-    }
+    // Get a reference to the goal node. Errors if the goal node cannot be found or is none
+    let goal_node_container = match unsafe { NODES.get(goal_node_id) } {
+        None => return Err(AstarError::GoalNodeNotFound),
+        Some(node_container) => node_container.node.read().unwrap(),
+    };
 
-    let goal_node = nodes.get(goal_node_id);
-    if goal_node.is_none() || goal_node.unwrap().is_none() {
-        return Err(AstarError::GoalNodeNotFound);
-    }
+    let goal_node = match goal_node_container.as_ref() {
+        None => return Err(AstarError::GoalNodeNotFound),
+        Some(node) => node,
+    };
 
-    if start_node.unwrap().as_ref().unwrap().z != goal_node.unwrap().as_ref().unwrap().z {
+    if goal_node.z
+        != start_node_container
+            .node
+            .read()
+            .unwrap()
+            .as_ref()
+            .unwrap()
+            .z
+    {
         return Err(AstarError::NoPathFound);
     }
 
+    // Compute the shortest path between start node and goal node using A*
     let path = astar(
-        &start_node.unwrap().clone().unwrap(),
-        |node| node.successors(),
-        |node| node.distance(&goal_node.unwrap().clone().unwrap()),
-        |node| node == &goal_node.unwrap().clone().unwrap(),
+        start_node_container,
+        |node_container| {
+            if let Some(node) = node_container.node.read().unwrap().as_ref() {
+                node.successors()
+            } else {
+                Vec::new()
+            }
+        },
+        |node_container| {
+            if let Some(node) = node_container.node.read().unwrap().as_ref() {
+                node.distance(goal_node)
+            } else {
+                MAX
+            }
+        },
+        |node_container| {
+            if let Some(node) = node_container.node.read().unwrap().as_ref() {
+                node.distance(goal_node) == 0
+            } else {
+                false
+            }
+        },
     );
-    if path.is_none() {
-        return Err(AstarError::NoPathFound);
-    }
 
-    let (path, _) = path.unwrap();
-    Ok(path.into_iter().map(|node| node.unique_id).collect())
-}
+    // Extract a vector of node container from the path variable. Errors if no path was found
+    let path = match path {
+        None => return Err(AstarError::NoPathFound),
+        Some(path) => path.0,
+    };
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    #[allow(unused_must_use)]
-    fn test_register_node() {
-        let json = std::fs::read_to_string("tests/rsc/ai_nodes_info.json").unwrap();
-        register_nodes_(&json);
-        assert!(NODES.read().unwrap().len() != 0)
-    }
-
-    #[test]
-    #[allow(unused_must_use)]
-    fn test_remove_node() {
-        let json = std::fs::read_to_string("tests/rsc/ai_nodes_info.json").unwrap();
-        register_nodes_(&json);
-        assert!(remove_node_("15").is_ok())
-    }
-
-    #[test]
-    #[allow(unused_must_use)]
-    fn test_add_node() {
-        let json = std::fs::read_to_string("tests/rsc/ai_nodes_info.json").unwrap();
-        register_nodes_(&json);
-        let mut node_to_add = NODES.read().unwrap().get(18).unwrap().clone().unwrap();
-        node_to_add.unique_id = NODES.read().unwrap().len();
-        let node_to_add = serde_json::to_string(&node_to_add).unwrap();
-        assert!(add_node_(&node_to_add).is_ok())
-    }
+    // Map every nodecontainer to the unique id of its node, so it can be sent to byond
+    Ok(path
+        .into_iter()
+        .map(|node| node.node.read().unwrap().as_ref().unwrap().unique_id)
+        .collect())
 }
