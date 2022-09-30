fc6a83489707e28750fef080f8f8ea2ade36f649
Mothblocks <35135081+Mothblocks@users.noreply.github.com>
2022-09-29 20:15:42 -0700

    I forgot that the point this serves doesn't apply here

--- dmsrc/toml.dm
+++ dmsrc/toml.dm
@@ -7,9 +7,11 @@
 	else
 		CRASH(output["content"])
 
-#define rustg_raw_write_toml_file(value, path, prepend) json_decode(call(RUST_G, "toml_file_from_json")(json_encode(value), path, prepend) || "null")
+#define rustg_raw_toml_encode(value) json_decode(call(RUST_G, "toml_encode")(json_encode(value)))
 
-/proc/rustg_write_toml_file(value, path, prepend = "")
-	var/error = rustg_raw_write_toml_file(json_encode(value), path, prepend)
-	if (error)
-		CRASH(error)
+/proc/rustg_toml_encode(value)
+	var/list/output = rustg_raw_toml_encode(value)
+	if (output["success"])
+		return output["content"]
+	else
+		CRASH(output["content"])
--- src/toml.rs
+++ src/toml.rs
@@ -21,23 +21,22 @@ fn toml_file_to_json_impl(path: &str) -> Result<String> {
     )?)?)?)
 }
 
-byond_fn!(fn toml_file_from_json(value, path, prepend) {
+byond_fn!(fn toml_encode(value) {
     serde_json::to_string(
-        &match toml_file_from_json_impl(value, path, prepend) {
-            Ok(()) => "".to_owned(),
-            Err(error) => error.to_string(),
+        &match toml_encode_impl(value) {
+            Ok(value) => serde_json::json!({
+                "success": true, "content": value
+            }),
+
+            Err(error) => serde_json::json!({
+                "success": false, "content": error.to_string()
+            }),
         }
     ).ok()
 });
 
-fn toml_file_from_json_impl(value: &str, path: &str, prepend: &str) -> Result<()> {
-    let toml_value: toml_dep::Value = serde_json::from_str(value)?;
-    let mut toml_text = toml_dep::to_string(&toml_value)?;
-    if !prepend.is_empty() {
-        toml_text = format!("{prepend}\n{toml_text}");
-    }
-
-    std::fs::write(path, toml_text)?;
-
-    Ok(())
+fn toml_encode_impl(value: &str) -> Result<String> {
+    Ok(toml_dep::to_string_pretty(&serde_json::from_str::<
+        toml_dep::Value,
+    >(value)?)?)
 }
