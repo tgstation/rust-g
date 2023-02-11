e6d01eee28429c6d5aa929582a479737492cc89d
ZeWaka <zewakagamer@gmail.com>
2023-02-11 11:15:50 -0800

    Finish the gitoxide conversion for commit date

--- dmsrc/git.dm
+++ dmsrc/git.dm
@@ -1,3 +1,2 @@
 #define rustg_git_revparse(rev) RUSTG_CALL(RUST_G, "rg_git_revparse")(rev)
-#define rustg_git_revparseold(rev) RUSTG_CALL(RUST_G, "rg_git_revparseold")(rev)
 #define rustg_git_commit_date(rev) RUSTG_CALL(RUST_G, "rg_git_commit_date")(rev)
--- src/git.rs
+++ src/git.rs
@@ -1,36 +1,25 @@
 use chrono::{TimeZone, Utc};
-use git2::{Error, ErrorCode, Repository};
 use git_repository as git;
 
 thread_local! {
-    static REPO: Result<git::Repository, git::discover::Error> = git::discover(".");
-    static REPOSITORY: Result<Repository, Error> = Repository::open(".");
+    static REPOSITORY: Result<git::Repository, git::discover::Error> = git::discover(".");
 }
 
 byond_fn!(fn rg_git_revparse(rev) {
-    REPO.with(|rep| -> Result<String, String> {
+    REPOSITORY.with(|rep| -> Result<String, String> {
         let repo = rep.as_ref().map_err(|e| e.to_string())?;
         let object = repo.rev_parse_single(rev).map_err(|e| e.to_string())?;
         Ok(object.to_hex().to_string())
     }).ok()
 });
 
-byond_fn!(fn rg_git_revparseold(rev) {
-    REPOSITORY.with(|repo| -> Result<String, ErrorCode> {
-        let repo = repo.as_ref().map_err(Error::code)?;
-        let object = repo.revparse_single(rev).map_err(|e| e.code())?;
-        Ok(object.id().to_string())
-    }).ok()
-});
-
-// rustg_git_revparse("origin/master")
-
 byond_fn!(fn rg_git_commit_date(rev) {
-    REPOSITORY.with(|repo| -> Result<String, ErrorCode> {
-        let repo = repo.as_ref().map_err(Error::code)?;
-        let object = repo.revparse_single(rev).map_err(|e| e.code())?;
-        let commit = object.as_commit().ok_or(ErrorCode::GenericError)?;
-        let datetime = Utc.timestamp(commit.time().seconds(), 0);
+    REPOSITORY.with(|rep| -> Result<String, String> {
+        let repo = rep.as_ref().map_err(|e| e.to_string())?;
+        let object = repo.rev_parse_single(rev).map_err(|e| e.to_string())?;
+        let commit = object.object().map_err(|e| e.to_string())?.into_commit();
+        let time = commit.time().map_err(|e| e.to_string())?;
+        let datetime = Utc.timestamp(time.seconds().into(), 0);
         Ok(datetime.format("%F").to_string())
     }).ok()
 });
