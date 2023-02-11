3d73e66d69d7d33a0090b38541b497af7475a771
ZeWaka <zewakagamer@gmail.com>
2023-02-11 10:41:20 -0800

    Gitoxide-powered revparse testing

--- src/git.rs
+++ src/git.rs
@@ -1,17 +1,29 @@
 use chrono::{TimeZone, Utc};
 use git2::{Error, ErrorCode, Repository};
+use git_repository as git;
 
 thread_local! {
+    static REPO: Result<git::Repository, git::discover::Error> = git::discover(".");
     static REPOSITORY: Result<Repository, Error> = Repository::open(".");
 }
 
 byond_fn!(fn rg_git_revparse(rev) {
+    REPO.with(|rep| -> Result<String, String> {
+        let repo = rep.as_ref().map_err(|e| e.to_string())?;
+        let object = repo.rev_parse_single(rev).map_err(|e| e.to_string())?;
+        Ok(object.to_hex().to_string())
+    }).ok()
+});
+
+byond_fn!(fn rg_git_revparseold(rev) {
     REPOSITORY.with(|repo| -> Result<String, ErrorCode> {
         let repo = repo.as_ref().map_err(Error::code)?;
         let object = repo.revparse_single(rev).map_err(|e| e.code())?;
         Ok(object.id().to_string())
     }).ok()
 });
+
+// rustg_git_revparse("origin/master")
 
 byond_fn!(fn rg_git_commit_date(rev) {
     REPOSITORY.with(|repo| -> Result<String, ErrorCode> {
