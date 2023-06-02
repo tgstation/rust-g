a12d5f9af34bead208cabe50254a16a7c3ed8b78
ZeWaka <zewakagamer@gmail.com>
2023-06-01 23:22:15 -0700

    cleanup

--- src/git.rs
+++ src/git.rs
@@ -1,8 +1,7 @@
 use chrono::{TimeZone, Utc};
-use git_repository as git;
 
 thread_local! {
-    static REPOSITORY: Result<git::Repository, git::discover::Error> = git::discover(".");
+    static REPOSITORY: Result<gix::Repository, gix::discover::Error> = gix::discover(".");
 }
 
 byond_fn!(fn rg_git_revparse(rev) {
@@ -19,7 +18,7 @@ byond_fn!(fn rg_git_commit_date(rev) {
         let object = repo.rev_parse_single(rev).map_err(|e| e.to_string())?;
         let commit = object.object().map_err(|e| e.to_string())?.into_commit();
         let time = commit.time().map_err(|e| e.to_string())?;
-        let datetime = Utc.timestamp(time.seconds().into(), 0);
+        let datetime = Utc.timestamp_opt(time.seconds().into(), 0).earliest().unwrap();
         Ok(datetime.format("%F").to_string())
     }).ok()
 });
