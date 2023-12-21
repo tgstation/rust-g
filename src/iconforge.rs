88055c64d0f631dd3571d7acf5104539500e58b2
itsmeow <itsmeow@itsmeow.dev>
2023-12-21 11:50:21 -0500

    Huge cleanup

--- src/iconforge.rs
+++ src/iconforge.rs
@@ -15,12 +15,26 @@ use serde::{Serialize, Deserialize};
 use std::collections::HashMap;
 use std::sync::{Arc, Mutex};
 use tracy_full::{zone, frame};
-use once_cell::sync::OnceCell;
+use once_cell::sync::Lazy;
+static ICON_FILES: Lazy<Mutex<HashMap<String, Icon>>> = Lazy::new(Mutex::default);
+static ICON_STATES: Lazy<Mutex<HashMap<String, &mut DynamicImage>>> = Lazy::new(Mutex::default);
 
-fn icon_file_to_icon() -> &'static Mutex<HashMap<String, Icon>> {
-    static INSTANCE: OnceCell<Mutex<HashMap<String, Icon>>> = OnceCell::new();
-    INSTANCE.get_or_init(|| Mutex::new(HashMap::new()))
-}
+const SOUTH: u8 = 2;
+const NORTH: u8 = 1;
+const EAST: u8 = 4;
+const WEST: u8 = 8;
+const FOUR_DIRS: [u8; 4] = [SOUTH, NORTH, EAST, WEST];
+const SOUTHEAST: u8 = SOUTH | EAST; // 6
+const SOUTHWEST: u8 = SOUTH | WEST; // 10
+const NORTHEAST: u8 = NORTH | EAST; // 5
+const NORTHWEST: u8 = NORTH | WEST; // 9
+const EIGHT_DIRS: [u8; 8] = [SOUTH, NORTH, EAST, WEST, SOUTHEAST, SOUTHWEST, NORTHEAST, NORTHWEST];
+
+const DMI_ORDERING: [u8; 8] = EIGHT_DIRS;
+// This is an array mapping the DIR number from above to a position in DMIs, such that DIR_TO_INDEX[DIR] = DMI_ORDERING.indexof(DIR)
+// 255 is invalid.
+const DIR_TO_INDEX: [u8; 11] = [255, 1, 0, 255, 2, 6, 4, 255, 3, 7, 5];
+
 
 byond_fn!(fn iconforge_generate(file_path, spritesheet_name, sprites) {
     catch_panic(file_path, spritesheet_name, sprites).err()
@@ -28,9 +42,10 @@ byond_fn!(fn iconforge_generate(file_path, spritesheet_name, sprites) {
 
 
 byond_fn!(fn iconforge_generate_async(file_path, spritesheet_name, sprites) {
-    let file_path = file_path.to_owned();
-    let spritesheet_name = spritesheet_name.to_owned();
-    let sprites = sprites.to_owned();
+    // Take ownership before passing
+    let file_path = file_path;
+    let spritesheet_name = spritesheet_name;
+    let sprites = sprites;
     Some(jobs::start(move || {
         match catch_panic(&file_path, &spritesheet_name, &sprites) {
             Ok(o) => o.to_string(),
@@ -64,6 +79,16 @@ struct IconObject {
 	frame: u32,
 	moving: u8,
 	transform: Vec<Transform>
+}
+
+trait IcoString {
+    fn to_icostring() -> String;
+}
+
+impl IcoString for IconObject {
+    fn to_icostring() -> String {
+        return "".to_string(); // TODO implement this as as unique ID. Transforms need another trait
+    }
 }
 
 #[derive(Serialize, Deserialize, Clone)]
@@ -113,81 +138,35 @@ fn generate_spritesheet(file_path: &str, spritesheet_name: &str, sprites: &str) 
 
     let error: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
 
-    let size_to_images: Arc<Mutex<HashMap<String, Vec<DynamicImage>>>> = Arc::new(Mutex::new(HashMap::new()));
+    let size_to_images: Arc<Mutex<HashMap<String, Vec<&mut DynamicImage>>>> = Arc::new(Mutex::new(HashMap::new()));
     let sprites_map: HashMap<String, IconObject> = serde_json::from_str::<HashMap<String, IconObject>>(sprites)?;
     let sprites_objects: Arc<Mutex<HashMap<String, SpritesheetEntry>>> = Arc::new(Mutex::new(HashMap::new()));
 
+    // Pre-load all the DMIs now.
     sprites_map.par_iter().for_each(|sprite_entry| {
         zone!("sprite_to_icons");
         let (_, icon) = sprite_entry;
         icon_to_icons(icon).par_iter().for_each(|icon| {
-            zone!("icon_to_dmi");
-            let icon_path = icon.icon_file.to_owned();
-            {
-                zone!("check_dmi_exists");
-                // scope-in so the lock does not persist during DMI read
-                if icon_file_to_icon().lock().unwrap().contains_key(&icon_path) {
-                    return;
-                }
-            }
-            let reader = BufReader::new(File::open(&icon_path).unwrap());
-            let dmi: Option<Icon>;
-            {
-                zone!("parse_dmi");
-                dmi = Icon::load(reader).ok();
-            }
-            if dmi.is_none() {
-                error.lock().unwrap().push(format!("Invalid DMI: {}", icon_path));
+            if let Err(err) = icon_to_dmi(icon) {
+                error.lock().unwrap().push(err);
                 return;
-            }
-            {
-                zone!("insert_dmi");
-                icon_file_to_icon().lock().unwrap().insert(icon_path, dmi.unwrap().to_owned());
             }
         });
     });
 
-
-
+    // Pick the specific icon states out of the DMI
     sprites_map.par_iter().for_each(|sprite_entry| {
         zone!("map_sprite");
         let (sprite_name, icon) = sprite_entry;
-        let parsed_icon = icon_file_to_icon().lock().unwrap().get(&icon.icon_file).unwrap().to_owned();
-        let mut matched_state: Option<IconState> = Option::None;
-        for icon_state in parsed_icon.states {
-            if icon_state.name == icon.icon_state {
-                matched_state = Option::Some(icon_state.clone());
-                break;
-            }
-        }
-        if matched_state.is_none() {
-            error.lock().unwrap().push(format!("Could not find associated icon state {} for {}", icon.icon_state, sprite_name));
+
+        // get DynamicImage
+        let image_result = icon_to_image(icon, sprite_name);
+        if image_result.is_err() {
+            error.lock().unwrap().push(image_result.unwrap_err());
             return;
         }
-        let state = matched_state.unwrap();
-        if !( if icon.dir == 2 { state.dirs >= 1 } else { state.dirs >= 4 } && state.frames >= icon.frame ) {
-            error.lock().unwrap().push(format!("Could not find associated dir or frame dir: {} frame: {} in {} icon_state - dirs: {} frames: {}", icon.dir, icon.frame, sprite_name, state.dirs, state.frames));
-            return;
-        }
-        let mut icon_idx: u32 = 0;
-        if state.dirs == 4 {
-            icon_idx = match icon.dir {
-                 2 => 0, // South
-                1 => 1, // North
-                4 => 2, // East
-                8 => 3, // West
-                _ => 0,
-            }
-        } else if state.dirs != 1 {
-            error.lock().unwrap().push(format!("Unsupported dirs size of {} in {} state: {} for sprite {}", state.dirs, icon.icon_file, icon.icon_state, sprite_name));
-            return;
-        }
-        if state.frames > 1 {
-            // Add one so zero scales properly
-            icon_idx = (icon_idx + 1) * icon.frame - 1
-        }
-        let image: &DynamicImage = state.images.get(usize::try_from(icon_idx).unwrap()).unwrap();
-        let mut cloned_image: DynamicImage = image.clone();
+        let image = image_result.unwrap();
+
         // apply transforms here
 
         for transform in &icon.transform {
@@ -195,36 +174,38 @@ fn generate_spritesheet(file_path: &str, spritesheet_name: &str, sprites: &str) 
                 Transform::BlendColorTransform { color, blend_mode } => {
                     let mutator = mutate(*blend_mode);
                     let color_parts = decode_hex(color).unwrap();
-                    for x in 0..cloned_image.width() {
-                        for y in 0..cloned_image.height() {
-                            let rgba = cloned_image.get_pixel(x, y).to_rgba();
-                            cloned_image.put_pixel(x, y, blend(rgba, [color_parts[0], color_parts[1], color_parts[2]], mutator))
+                    for x in 0..image.width() {
+                        for y in 0..image.height() {
+                            let rgba = image.get_pixel(x, y).to_rgba();
+                            image.put_pixel(x, y, blend(rgba, [color_parts[0], color_parts[1], color_parts[2]], mutator))
                         }
                     }
                 },
                 Transform::BlendIconTransform { icon, blend_mode } => {
+                    /*
                     let mutator = mutate(*blend_mode);
                     let color_parts = decode_hex(color).unwrap();
-                    for x in 0..cloned_image.width() {
-                        for y in 0..cloned_image.height() {
-                            let rgba = cloned_image.get_pixel(x, y).to_rgba();
-                            cloned_image.put_pixel(x, y, blend(rgba, [color_parts[0], color_parts[1], color_parts[2]], mutator))
+                    for x in 0..image.width() {
+                        for y in 0..image.height() {
+                            let rgba = image.get_pixel(x, y).to_rgba();
+                            image.put_pixel(x, y, blend(rgba, [color_parts[0], color_parts[1], color_parts[2]], mutator))
                         }
                     }
+                    */
                 },
                 Transform::ScaleTransform { width, height } => {
-                    cloned_image.resize_exact(*width, *height, image::imageops::FilterType::Nearest);
+                    *image = image.resize_exact(*width, *height, image::imageops::FilterType::Nearest);
                 }
                 Transform::CropTransform { x1, y1, x2, y2 } => {
-                    //cloned_image = cloned_image.crop_imm(x1, y1, x2 - x1, y2 - y1)
+                    //*image = image.crop_imm(x1, y1, x2 - x1, y2 - y1)
                 }
             }
         }
 
-        let size_id = format!("{}x{}", cloned_image.width(), cloned_image.height());
+        let size_id = format!("{}x{}", image.width(), image.height());
         let mut size_map = size_to_images.lock().unwrap();
         let vec = (*size_map).entry(size_id.to_owned()).or_insert(Vec::new());
-        vec.push(cloned_image);
+        vec.push(image);
         sprites_objects.lock().unwrap().insert(sprite_name.to_owned(), SpritesheetEntry {
             size_id: size_id.to_owned(),
             position: u32::try_from(vec.len()).unwrap() - 1
@@ -257,7 +238,7 @@ fn generate_spritesheet(file_path: &str, spritesheet_name: &str, sprites: &str) 
         }
     });
 
-    let sizes: Vec<String> = size_to_images.lock().unwrap().clone().into_keys().collect();
+    let sizes: Vec<String> = size_to_images.lock().unwrap().iter().map(|(k, _v)| k).cloned().collect();
 
     let returned = Returned {
         sizes: sizes,
@@ -267,19 +248,112 @@ fn generate_spritesheet(file_path: &str, spritesheet_name: &str, sprites: &str) 
     Ok(serde_json::to_string::<Returned>(&returned).unwrap())
 }
 
-fn icon_to_icons(icon: &IconObject) -> Vec<IconObject> {
-    let mut icons: Vec<IconObject> = Vec::new();
-    icons.push(icon.to_owned());
+/// Takes in an icon and gives a list of nested icons. Also returns a reference to the provided icon in the list.
+fn icon_to_icons(icon: &IconObject) -> Vec<&IconObject> {
+    let mut icons: Vec<&IconObject> = Vec::new();
+    icons.push(icon);
     for transform in &icon.transform {
         match transform {
             Transform::BlendIconTransform { icon, .. } => {
-                let nested = icon_to_icons(&icon);
-                icons.extend(nested.to_owned());
+                let nested = icon_to_icons(icon);
+                for icon in nested {
+                    icons.push(icon)
+                }
             }
             _ => {}
         }
     }
     return icons;
+}
+
+/// Given an IconObject, returns a DMI Icon structure and caches it.
+fn icon_to_dmi(icon: &IconObject) -> Result<&Icon, String> {
+    zone!("icon_to_dmi");
+    let icon_path: String = icon.icon_file;
+    {
+        zone!("check_dmi_exists");
+        // scope-in so the lock does not persist during DMI read
+        let found_icon = ICON_FILES.lock().unwrap().get(&icon_path);
+        if found_icon.is_some() {
+            return Ok(found_icon.unwrap());
+        }
+    }
+    let reader = BufReader::new(File::open(&icon_path).unwrap());
+    let dmi: Option<Icon>;
+    {
+        zone!("parse_dmi");
+        dmi = Icon::load(reader).ok();
+    }
+    if dmi.is_none() {
+        return Err(format!("Invalid DMI: {}", icon_path));
+    }
+    {
+        zone!("insert_dmi");
+        let my_dmi = dmi.unwrap();
+        // cache it for later.
+        // Ownership is given to the hashmap
+        ICON_FILES.lock().unwrap().insert(icon_path,my_dmi);
+        return Ok(&my_dmi);
+    }
+}
+
+fn icon_to_image<'a>(icon: &'a IconObject, sprite_name: &String) -> Result<&'a mut DynamicImage, String> {
+    {
+        zone!("check_dynamicimage_exists");
+        // scope-in so the lock does not persist during DMI read
+        let found_icon = ICON_STATES.lock().unwrap().get(&icon.to_icostring());
+        if found_icon.is_some() {
+            return Ok(*found_icon.unwrap())
+        }
+    }
+    let result = icon_to_dmi(icon);
+    if result.is_err()  {
+        return Err(result.unwrap_err());
+    }
+    let dmi = result.unwrap();
+    let mut matched_state: Option<&IconState> = Option::None;
+    {
+        zone!("match_icon_state");
+        for icon_state in dmi.states {
+            if icon_state.name == icon.icon_state {
+                matched_state = Option::Some(&icon_state);
+                break;
+            }
+        }
+    }
+    if matched_state.is_none() {
+        return Err(format!("Could not find associated icon state {} for {}", icon.icon_state, sprite_name));
+    }
+    let state = matched_state.unwrap();
+    {
+        zone!("determine_icon_state_validity");
+        if state.frames < icon.frame {
+            return Err(format!("Could not find associated frame: {} in {} icon_state {} - dirs: {} frames: {}", icon.frame, sprite_name, icon.icon_state, state.dirs, state.frames));
+        }
+        if (state.dirs == 1 && icon.dir != SOUTH)
+        || (state.dirs == 4 && !FOUR_DIRS.contains(&icon.dir))
+        || (state.dirs == 8 && !EIGHT_DIRS.contains(&icon.dir)) {
+            return Err(format!("Invalid dir {} or size of dirs {} in {} state: {} for sprite {}", icon.dir, state.dirs, icon.icon_file, icon.icon_state, sprite_name));
+        }
+
+    }
+    let icon_index = DIR_TO_INDEX.get(icon.dir as usize);
+    if icon_index.is_none() || *icon_index.unwrap() == 255 {
+        return Err(format!("Invalid dir {} or size of dirs {} in {} state: {} for sprite {}", icon.dir, state.dirs, icon.icon_file, icon.icon_state, sprite_name));
+    }
+    let mut icon_idx: u32 = *icon_index.unwrap() as u32;
+    if icon.frame > 1 {
+        // Add one so zero scales properly
+        icon_idx = (icon_idx + 1) * icon.frame - 1
+    }
+    let image: &mut DynamicImage = state.images.get_mut(icon_idx as usize).unwrap();
+    {
+        zone!("insert_dynamicimage");
+        // cache it for later.
+        // Ownership is given to the hashmap
+        ICON_STATES.lock().unwrap().insert(icon.to_icostring(), image);
+    }
+    return Ok(image);
 }
 
 fn mutate(blend_mode: u8) -> fn(u8, u8) -> u8 {
