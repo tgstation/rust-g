030b3a6af16a9044f9fc75a58534ffdb948c1e7e
Kyle Spier-Swenson <kyleshome@gmail.com>
2024-08-09 00:23:18 -0700

    appease the travis gods

--- src/lib.rs
+++ src/lib.rs
@@ -56,4 +56,6 @@ pub mod url;
 pub mod worleynoise;
 
 #[cfg(all(not(target_pointer_width = "32"), not(feature = "allow_non_32bit")))]
-compile_error!("Compiling for non-32bit is not allowed without enabling the `allow_non_32bit` feature.");
+compile_error!(
+    "Compiling for non-32bit is not allowed without enabling the `allow_non_32bit` feature."
+);
