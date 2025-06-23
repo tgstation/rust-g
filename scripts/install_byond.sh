7756db74341733e4e3402607081c4b7c72f8c1b8
ZeWaka <zewakagamer@gmail.com>
2025-06-22 21:03:14 -0700

    this

--- scripts/install_byond.sh
+++ scripts/install_byond.sh
@@ -9,9 +9,12 @@ else
     rm -rf "$HOME/BYOND"
     mkdir -p "$HOME/BYOND"
     cd "$HOME/BYOND"
-    if ! curl --connect-timeout 3 "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "GitHub Actions/1.0"; then
-        echo "BYOND download failed, using mirror"
-        curl "https://spacestation13.github.io/byond-builds/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "GitHub Actions/1.0"
+    if ! curl --connect-timeout 3 --max-time 10 "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "GitHub Actions/1.0"; then
+        echo "BYOND download failed or timed out, using mirror"
+        if ! curl --connect-timeout 3 --max-time 10 "https://spacestation13.github.io/byond-builds/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "GitHub Actions/1.0"; then
+            echo "Mirror download failed too :("
+            exit 1
+        fi
     fi
     unzip byond.zip
     rm byond.zip
