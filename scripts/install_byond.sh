db89d1bdd7827998ed1536ea1d68ad0511b6d8e2
ZeWaka <zewakagamer@gmail.com>
2025-06-22 20:59:07 -0700

    set up mirror to work

--- scripts/install_byond.sh
+++ scripts/install_byond.sh
@@ -9,7 +9,10 @@ else
     rm -rf "$HOME/BYOND"
     mkdir -p "$HOME/BYOND"
     cd "$HOME/BYOND"
-    curl "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "GitHub Actions/1.0"
+    if ! curl --connect-timeout 3 "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "GitHub Actions/1.0"; then
+        echo "BYOND download failed, using mirror"
+        curl "https://spacestation13.github.io/byond-builds/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "GitHub Actions/1.0"
+    fi
     unzip byond.zip
     rm byond.zip
     cd byond
