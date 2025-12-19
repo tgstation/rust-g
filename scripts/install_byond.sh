60c14bb5c2c431e40567b10d7d43d2ccddb880e2
ZeWaka <zewakagamer@gmail.com>
2025-12-19 00:10:37 -0800

    test on windows (maybe works)

--- scripts/install_byond.sh
+++ scripts/install_byond.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 set -euo pipefail
 
-if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt;
+# Detect OS
+if [[ "$OSTYPE" == "win32" ]]; then
+    IS_WINDOWS=1
+    BYOND_SUFFIX=""
+else
+    IS_WINDOWS=0
+    BYOND_SUFFIX="_linux"
+fi
+
+if [ -d "$HOME/BYOND/byond/bin" ] && grep -Fxq "${BYOND_MAJOR}.${BYOND_MINOR}" $HOME/BYOND/version.txt 2>/dev/null;
 then
     echo "Using cached directory."
 else
@@ -9,17 +18,24 @@ else
     rm -rf "$HOME/BYOND"
     mkdir -p "$HOME/BYOND"
     cd "$HOME/BYOND"
-    if ! curl --connect-timeout 2 --max-time 8 "https://spacestation13.github.io/byond-builds/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "GitHub Actions/1.0"; then
+
+    if ! curl --connect-timeout 2 --max-time 8 "https://spacestation13.github.io/byond-builds/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond${BYOND_SUFFIX}.zip" -o byond.zip -A "GitHub Actions/1.0"; then
         echo "Mirror download failed, falling back to byond.com"
-        if ! curl --connect-timeout 2 --max-time 8 "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond_linux.zip" -o byond.zip -A "GitHub Actions/1.0"; then
+        if ! curl --connect-timeout 2 --max-time 8 "http://www.byond.com/download/build/${BYOND_MAJOR}/${BYOND_MAJOR}.${BYOND_MINOR}_byond${BYOND_SUFFIX}.zip" -o byond.zip -A "GitHub Actions/1.0"; then
             echo "BYOND download failed too :("
             exit 1
         fi
     fi
+
     unzip byond.zip
     rm byond.zip
-    cd byond
-    make here
+
+    if [ "$IS_WINDOWS" -eq 0 ]; then
+        cd byond
+        make here
+        cd ..
+    fi
+
     echo "$BYOND_MAJOR.$BYOND_MINOR" > "$HOME/BYOND/version.txt"
     cd ~/
 fi
